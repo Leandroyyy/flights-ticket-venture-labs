a5ceecb7e1607ef7c2c0f63c65e6983461915775
Leandroyyy <leandro.cavallari123@gmail.com>
2022-09-04 14:09:39 -0300

    adicionado sqlite

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT || 3000
+ 
 app.listen(PORT, ()=>{
     console.log('Servidor iniciado na porta: '+ PORT)
 })
